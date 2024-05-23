502f9733fdae204164f120a421dc0f1c7de7f087
Marcus Nash <marcusnash@Marcuss-Mac-mini.local>
2024-05-23 11:04:40 -0400

    clear buttons

--- lib/main.dart
+++ lib/main.dart
@@ -5,117 +5,220 @@ import 'package:flutter/material.dart';
 import 'package:wp_chessboard/components/pieces.dart';
 import 'package:wp_chessboard/wp_chessboard.dart';
 
-
-
 void main() {
   runApp(MaterialApp(home: const MyApp()));
 }
 
 class MyApp extends StatefulWidget {
-
   const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
-class Spy { 
-  
-  Spy({required this.title, required this.piece}); 
+class Spy {
+  Spy({required this.title, required this.position, required this.piece});
 
   String title;
+  String position;
   dynamic piece;
 }
 
 class _MyAppState extends State<MyApp> {
   final controller = WPChessboardController();
-  Chess.Chess chess = Chess.Chess.fromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+  Chess.Chess chess = Chess.Chess.fromFEN(
+      "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
   List<List<int>>? lastMove;
 
   void initState() {
     setState(() {
-      chess = Chess.Chess.fromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+      chess = Chess.Chess.fromFEN(
+          "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     });
     update();
   }
+
   List<dynamic> spec_list = [
-          Spy(title: "1", piece: null),
-          Spy(title: "2", piece: null),
-          Spy(title: "3", piece: null)  ];  //  -------------------------LIST--------------------------------- 
+    Spy(title: "1", position: "", piece: null),
+    Spy(title: "2", position: "", piece: null),
+    Spy(title: "3", position: "", piece: null)
+  ]; //  -------------------------LIST---------------------------------
 
   // not working on drop
   Widget squareBuilder(SquareInfo info) {
-    Color fieldColor = (info.index + info.rank) % 2 == 0 ? Colors.grey.shade200 : Colors.grey.shade600;
+    Color fieldColor = (info.index + info.rank) % 2 == 0
+        ? Colors.grey.shade200
+        : Colors.grey.shade600;
     Color overlayColor = Colors.transparent;
 
-    if (lastMove != null ) {
-      if (lastMove!.first.first == info.rank && lastMove!.first.last == info.file) {
+    if (lastMove != null) {
+      if (lastMove!.first.first == info.rank &&
+          lastMove!.first.last == info.file) {
         overlayColor = Colors.blueAccent.shade400.withOpacity(0.4);
-      } else if (lastMove!.last.first == info.rank && lastMove!.last.last == info.file) {
+      } else if (lastMove!.last.first == info.rank &&
+          lastMove!.last.last == info.file) {
         overlayColor = Colors.blueAccent.shade400.withOpacity(0.87);
       }
     }
 
     return Container(
-      color: fieldColor,
-      width: info.size,
-      height: info.size,
-      child: AnimatedContainer(
-        color: overlayColor,
+        color: fieldColor,
         width: info.size,
         height: info.size,
-        duration: const Duration(milliseconds: 200),
-      )
-    );
+        child: AnimatedContainer(
+          color: overlayColor,
+          width: info.size,
+          height: info.size,
+          duration: const Duration(milliseconds: 200),
+        ));
   }
 
   void onPieceStartDrag(SquareInfo square, String piece) {
     showHintFields(square, piece);
   }
 
-  void onPieceTap(SquareInfo square, String piece) {
+  void clearList() {
+    setState(() {
+      spec_list = [
+        Spy(title: "1", position: "", piece: null),
+        Spy(title: "2", position: "", piece: null),
+        Spy(title: "3", position: "", piece: null)
+      ];
+    });
+  }
 
- 
-    developer.log('Square => ${square} || Piece => ${piece}');    //  ---------------------------------------------------------- 
+  void onPieceTap(SquareInfo square, String piece) {
+    developer.log(
+        'Square => ${square} || Piece => ${piece}'); //  ----------------------------------------------------------
 
     String spy = '$square-$piece';
-    Spy spyObj = new Spy(title: spy, piece: BlackBishop(size: 50));
-    spec_list.removeAt(2);  
+    Spy spyObj = new Spy(title: "", position: "", piece: null);
+
+    switch (piece) {
+      case "K":
+        // do something
+        spyObj = new Spy(
+            title: "White King",
+            position: '$square',
+            piece: WhiteKing(size: 50));
+        break;
+      case "Q":
+        // do something else
+        spyObj = new Spy(
+            title: "White Queen",
+            position: '$square',
+            piece: WhiteQueen(size: 50));
+        break;
+      case "B":
+        // do something else
+        spyObj = new Spy(
+            title: "White Bishop",
+            position: '$square',
+            piece: WhiteBishop(size: 50));
+        break;
+      case "N":
+        // do something else
+        spyObj = new Spy(
+            title: "White Knight",
+            position: '$square',
+            piece: WhiteKnight(size: 50));
+        break;
+      case "R":
+        // do something else
+        spyObj = new Spy(
+            title: "White Rook",
+            position: '$square',
+            piece: WhiteRook(size: 50));
+        break;
+      case "P":
+        // do something else
+        spyObj = new Spy(
+            title: "White Pawn",
+            position: '$square',
+            piece: WhitePawn(size: 50));
+        break;
+
+      case "k":
+        // do something
+        spyObj = new Spy(
+            title: "Black King",
+            position: '$square',
+            piece: BlackKing(size: 50));
+        break;
+      case "q":
+        // do something else
+        spyObj = new Spy(
+            title: "Black Queen",
+            position: '$square',
+            piece: BlackQueen(size: 50));
+        break;
+      case "b":
+        // do something else
+        spyObj = new Spy(
+            title: "Black Bishop",
+            position: '$square',
+            piece: BlackBishop(size: 50));
+        break;
+      case "n":
+        // do something else
+        spyObj = new Spy(
+            title: "Black Knight",
+            position: '$square',
+            piece: BlackKnight(size: 50));
+        break;
+      case "r":
+        // do something else
+        spyObj = new Spy(
+            title: "Black Rook",
+            position: '$square',
+            piece: BlackRook(size: 50));
+        break;
+      case "p":
+        // do something else
+        spyObj = new Spy(
+            title: "Black Pawn",
+            position: '$square',
+            piece: BlackPawn(size: 50));
+        break;
+    }
+
+    spec_list.removeAt(2);
     spec_list = [spyObj, ...spec_list];
 
-            setState(() {
-           spec_list= spec_list;
-        });
+    setState(() {
+      spec_list = spec_list;
+    });
 
-    developer.log('Spec list $spec_list');  
+    developer.log('Spec list $spec_list');
     showDialog(
-      context: context, 
-      builder: (context) => AlertDialog(
-        title: Text('Piece Data'),
-        content:  Text('Square => ${square} || Piece => ${piece}'),
-        
-      ));
-    
-    
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text('Piece Data'),
+              content: Text('Square => ${square} || Piece => ${piece}'),
+            ));
+
     // if (controller.hints.key == square.index.toString()) {
     //   controller.setHints(HintMap());
     //   return;
     // }
     // showHintFields(square, piece);
   }
-  
+
   void showHintFields(SquareInfo square, String piece) {
-    final moves = chess.generate_moves({ 'square': square.toString() });
+    final moves = chess.generate_moves({'square': square.toString()});
     final hintMap = HintMap(key: square.index.toString());
     for (var move in moves) {
       String to = move.toAlgebraic;
       int rank = to.codeUnitAt(1) - "1".codeUnitAt(0) + 1;
       int file = to.codeUnitAt(0) - "a".codeUnitAt(0) + 1;
 
-      hintMap.set(rank, file, (size) => MoveHint(
-        size: size,
-        onPressed: () => doMove(move),
-      ));
+      hintMap.set(
+          rank,
+          file,
+          (size) => MoveHint(
+                size: size,
+                onPressed: () => doMove(move),
+              ));
     }
     controller.setHints(hintMap);
   }
@@ -125,11 +228,11 @@ class _MyAppState extends State<MyApp> {
   }
 
   void onPieceDrop(PieceDropEvent event) {
-    chess.move({ "from": event.from.toString(), "to": event.to.toString() });
-    
+    chess.move({"from": event.from.toString(), "to": event.to.toString()});
+
     lastMove = [
-      [ event.from.rank, event.from.file ],
-      [ event.to.rank, event.to.file ]
+      [event.from.rank, event.from.file],
+      [event.to.rank, event.to.file]
     ];
 
     update(animated: false);
@@ -137,28 +240,31 @@ class _MyAppState extends State<MyApp> {
 
   void doMove(Chess.Move move) {
     chess.move(move);
-    
+
     int rankFrom = move.fromAlgebraic.codeUnitAt(1) - "1".codeUnitAt(0) + 1;
     int fileFrom = move.fromAlgebraic.codeUnitAt(0) - "a".codeUnitAt(0) + 1;
     int rankTo = move.toAlgebraic.codeUnitAt(1) - "1".codeUnitAt(0) + 1;
     int fileTo = move.toAlgebraic.codeUnitAt(0) - "a".codeUnitAt(0) + 1;
     lastMove = [
-      [ rankFrom, fileFrom ],
-      [ rankTo, fileTo ]
+      [rankFrom, fileFrom],
+      [rankTo, fileTo]
     ];
 
     update();
   }
+
   void setDefaultFen() {
     setState(() {
-      chess = Chess.Chess.fromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+      chess = Chess.Chess.fromFEN(
+          "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     });
     update();
   }
 
   void setRandomFen() {
     setState(() {
-      chess = Chess.Chess.fromFEN("3bK3/4B1P1/3p2N1/1rp3P1/2p2p2/p3n3/P5k1/6q1 w - - 0 1");
+      chess = Chess.Chess.fromFEN(
+          "3bK3/4B1P1/3p2N1/1rp3P1/2p2p2/p3n3/P5k1/6q1 w - - 0 1");
     });
     update();
   }
@@ -174,10 +280,9 @@ class _MyAppState extends State<MyApp> {
         to: SquareLocation.fromString("c3"),
       ),
       Arrow(
-        from: SquareLocation.fromString("g1"),
-        to: SquareLocation.fromString("f3"),
-        color: Colors.red
-      )
+          from: SquareLocation.fromString("g1"),
+          to: SquareLocation.fromString("f3"),
+          color: Colors.red)
     ]);
   }
 
@@ -199,13 +304,13 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WPChessboard Demo',
-      home: Scaffold(
-        body: Builder(builder: (context) {
+        title: 'WPChessboard Demo',
+        home: Scaffold(body: Builder(
+          builder: (context) {
             final double size = MediaQuery.of(context).size.shortestSide;
 
-        int speclistlen = spec_list.length;
-        
+            int speclistlen = spec_list.length;
+
             return Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -222,9 +327,8 @@ class _MyAppState extends State<MyApp> {
                   turnTopPlayerPieces: false,
                   ghostOnDrag: true,
                   dropIndicator: DropIndicatorArgs(
-                    size: size / 2,
-                    color: Colors.lightBlue.withOpacity(0.24)
-                  ),
+                      size: size / 2,
+                      color: Colors.lightBlue.withOpacity(0.24)),
                   pieceMap: PieceMap(
                     K: (size) => WhiteKing(size: size),
                     Q: (size) => WhiteQueen(size: size),
@@ -242,64 +346,84 @@ class _MyAppState extends State<MyApp> {
                 ),
                 const SizedBox(height: 24),
                 Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                            Container(
-                                padding: EdgeInsets.only(left: 8.0),
-                                width: 120,
-                                height: 200,
-                                child:Column(
-                                  children: [
-                                    ElevatedButton(onPressed: () {}, child: Text("${spec_list[0].title}")),
-                                    const SizedBox(height: 24),
-                                    ElevatedButton(onPressed: () {}, child: spec_list[0].piece != null ? spec_list[0].piece : null)
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                            padding: EdgeInsets.only(left: 8.0),
+                            width: 120,
+                            height: 200,
+                            child: Column(children: [
+                              ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text("${spec_list[0].title}",
+                                      textAlign: TextAlign.center)),
+                              const SizedBox(height: 24),
+                              ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text("${spec_list[1].position}")),
+                              const SizedBox(height: 24),
+                              ElevatedButton(
+                                  onPressed: () {},
+                                  child: spec_list[0].piece != null
+                                      ? spec_list[0].piece
+                                      : null)
+                            ])
 
-      
-                                   ]
-                                
-                                )
+                            //  ElevatedButton(onPressed: () {}, child: Text("${spec_list[0].title}"))
+                            ),
+                        Container(
+                            padding: EdgeInsets.only(left: 8.0),
+                            width: 120,
+                            height: 200,
+                            child: Column(children: [
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Text("${spec_list[1].title}",
+                                    textAlign: TextAlign.center),
+                              ),
+                              const SizedBox(height: 24),
+                              ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text("${spec_list[1].position}")),
+                              const SizedBox(height: 24),
+                              ElevatedButton(
+                                  onPressed: () {},
+                                  child: spec_list[1].piece != null
+                                      ? spec_list[1].piece
+                                      : null)
+                            ])),
+                        //ElevatedButton(onPressed: () {}, child: Text("${spec_list[1].title}"))),
+                        Container(
+                            padding: EdgeInsets.only(left: 8.0),
+                            width: 120,
+                            height: 200,
+                            child: Column(children: [
+                              ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text("${spec_list[2].title}",
+                                      textAlign: TextAlign.center)),
+                              const SizedBox(height: 24),
+                              ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text("${spec_list[2].position}")),
+                              const SizedBox(height: 24),
+                              ElevatedButton(
+                                  onPressed: () {},
+                                  child: spec_list[2].piece != null
+                                      ? spec_list[2].piece
+                                      : null)
+                            ]))
+                      ]
 
-                                    //  ElevatedButton(onPressed: () {}, child: Text("${spec_list[0].title}"))
-                                    ),
-                             Container(
-                                padding: EdgeInsets.only(left: 8.0),
-                                width: 120,
-                                height: 200,
-                                child: Column(
-                                  children: [
-                                    ElevatedButton(onPressed: () {}, child: Text("${spec_list[1].title}")),
-                                    const SizedBox(height: 24),
-                                    ElevatedButton(onPressed: () {}, child: spec_list[1].piece != null ? spec_list[1].piece : null)
-
-      
-                                   ]
-                                
-                                )
-
-                                ),
-                                    //ElevatedButton(onPressed: () {}, child: Text("${spec_list[1].title}"))),
-                             Container(
-                                padding: EdgeInsets.only(left: 8.0),
-                                width: 120,
-                                height: 200,
-                                child: Column(
-                                  children: [
-                                    ElevatedButton(onPressed: () {}, child: Text("${spec_list[2].title}")),
-                                    const SizedBox(height: 24),
-                                   ElevatedButton(onPressed: () {}, child: spec_list[2].piece != null ? spec_list[2].piece : null)
-
-      
-                                   ]
-                                )
-                              )
-                                    
-                          ]
-                  
-                  //spec_list.getRange(0, speclistlen).map((e) => Text('$e')).toList(),
-                ),
+                      //spec_list.getRange(0, speclistlen).map((e) => Text('$e')).toList(),
+                      ),
                 ),
                 const SizedBox(height: 24),
+                TextButton(
+                  onPressed: clearList,
+                  child: const Text("Clear"),
+                ),
                 // TextButton(
                 //   onPressed: setDefaultFen,
                 //   child: const Text("Set default Fen"),
@@ -323,8 +447,6 @@ class _MyAppState extends State<MyApp> {
               ],
             );
           },
-        )
-      )
-    );
+        )));
   }
 }
